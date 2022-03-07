fecbc16b59233e56c86debf75a73521d82da2e11
evertonrf <64816217+evertonrf@users.noreply.github.com>
2022-03-07 10:55:45 -0300

    quebrar lampada

--- lampada.js
+++ lampada.js
@@ -8,7 +8,11 @@ function lampOn() {
 function lampOff() {
     lamp.src = './img/desligada.jpg'
 }
-
+function lampBroken() {
+    lamp.src = './img/quebrada.jpg'
+}
 turnOn.addEventListener('click', lampOn);
 turnOff.addEventListener('click', lampOff);
 lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+lamp.addEventListener('dblclick', lampBroken);
